1044962c8aad803b6d07906d584724dd7ea7a46f
yellowandrew <68777254@qq.com>
2022-05-27 17:38:29 +0800

    Update NetServer.cs
    clean code

--- _Frame/core/server_side/NetServer.cs
+++ _Frame/core/server_side/NetServer.cs
@@ -13,13 +13,7 @@ public class NetServer
     protected int maxclient = 100;
     public NetServer()
     {
-        INIParser parser = new INIParser();
-        parser.Open(Application.streamingAssetsPath + "/game.cfg");
-        port = parser.ReadValue("server", "port", port);
-        maxclient = parser.ReadValue("server", "maxclient", maxclient);
-        buffer = parser.ReadValue("server", "buffer", buffer);
-        tick = parser.ReadValue("server", "tick", tick); 
-        parser.Close();
+       
     }
 
     public void LoadConfig(object cfg) {
